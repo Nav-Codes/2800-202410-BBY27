50cbecab6ffd4837fc6338148c3684bdd17e4494
Gasd123 <gdupaya@my.bcit.ca>
2024-05-24 12:58:04 -0700

    Implemented the feature where the progress changes with the random goal.

--- public/js/goalsHome.js
+++ public/js/goalsHome.js
@@ -1,23 +1,16 @@
 const progress = document.querySelector('.progress-done');
-const input = document.querySelector('.input');
-const maxInput = document.querySelector('.maxInput');
-let finalValue = 0;
-let maxValue = 0;
 
-function changeWidth() {
-    if (maxValue > 0) {
-        const widthPercentage = (finalValue / maxValue) * 100;
-        progress.style.width = `${widthPercentage}%`;
-        progress.innerText = `${Math.ceil(widthPercentage)}%`;
+function updateProgressBar() {
+    if (randomGoal) {
+        const finalValue = randomGoal[3];
+        const maxValue = randomGoal[0];
+        if (maxValue > 0) {
+            const widthPercentage = (finalValue / maxValue) * 100;
+            progress.style.width = `${widthPercentage}%`;
+            progress.innerText = `${Math.ceil(widthPercentage)}%`;
+        }
     }
 }
 
-input.addEventListener('keyup', function () {
-    finalValue = parseInt(input.value, 10) || 0;
-    changeWidth();
-});
-
-maxInput.addEventListener('keyup', function () {
-    maxValue = parseInt(maxInput.value, 10) || 0;
-    changeWidth();
-});
+// Initial render
+updateProgressBar();
