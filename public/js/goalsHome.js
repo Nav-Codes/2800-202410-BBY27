4ed54a26a15a66edb71cb60ad1b222dd02d45b52
Matthew Dodd <matthew_dodd_94@hotmail.com>
2024-05-27 13:25:38 -0700

    Made goals on the home page populate based on which goals are being tracked.

--- public/js/goalsHome.js
+++ public/js/goalsHome.js
@@ -1,16 +1,12 @@
-const progress = document.querySelector('.progress-done');
-
-function updateProgressBar() {
-    if (randomGoal) {
-        const finalValue = randomGoal[3];
-        const maxValue = randomGoal[0];
-        if (maxValue > 0) {
-            const widthPercentage = (finalValue / maxValue) * 100;
-            progress.style.width = `${widthPercentage}%`;
-            progress.innerText = `${Math.ceil(widthPercentage)}%`;
-        }
+// Function to update the progress bar for each goal
+function updateProgressBar(index, goal) {
+    const progress = document.querySelector(`.progress-done.progress-done${index}`);
+    const finalValue = goal[3];
+    const maxValue = goal[0];
+    if (maxValue > 0) {
+        const widthPercentage = (finalValue / maxValue) * 100;
+        progress.style.width = `${widthPercentage}%`;
+        progress.innerText = `${Math.ceil(widthPercentage)}%`;
     }
 }
 
-// Initial render
-updateProgressBar();
