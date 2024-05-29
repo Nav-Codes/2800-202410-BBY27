16befc68b1d0c0abc0d27e55b9e210a7fd1136df
Matthew Dodd <matthew_dodd_94@hotmail.com>
2024-05-28 17:07:08 -0700

    Moved JS from homeAuthenticated to own goalsHome.

--- public/js/goalsHome.js
+++ public/js/goalsHome.js
@@ -1,12 +1,17 @@
-// Function to update the progress bar for each goal
-function updateProgressBar(index, goal) {
-    const progress = document.querySelector(`.progress-done.progress-done${index}`);
-    const finalValue = goal[3];
-    const maxValue = goal[0];
-    if (maxValue > 0) {
-        const widthPercentage = (finalValue / maxValue) * 100;
-        progress.style.width = `${widthPercentage}%`;
-        progress.innerText = `${Math.ceil(widthPercentage)}%`;
+document.addEventListener("DOMContentLoaded", function() {
+    function updateProgressBar(index, goal) {
+        const progress = document.querySelector(`.progress-done.progress-done${index}`);
+        const finalValue = goal[3];
+        const maxValue = goal[0];
+        if (maxValue > 0) {
+            const widthPercentage = (finalValue / maxValue) * 100;
+            progress.style.width = `${widthPercentage}%`;
+            progress.innerText = `${Math.ceil(widthPercentage)}%`;
+        }
     }
-}
 
+    // Iterate through each goal and update the progress bar
+    goals.forEach((goal, index) => {
+        updateProgressBar(index, goal);
+    });
+});
